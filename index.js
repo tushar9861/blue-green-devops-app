0bb76430ab6f3a471993b7a5eace898aa371617e
tushar9861 <tusardas7788@gmail.com>
2026-03-22 03:30:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const server = http.createServer((req, res) => {
     res.end("Hello Engineers from Blue-Green Deployment v1");
 });
 
-// ✅ FIX HERE
 server.listen(3000, "0.0.0.0", () => {
     console.log("Server running on port 3000");
 });
